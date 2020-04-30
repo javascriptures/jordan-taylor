1371496f1e207ada163db58e29d5143fc5e7b54b
javascriptures <jtjavascripture@gmail.com>
2020-04-30 13:01:52 -0700

    Updates

---
+++ precache-manifest.00f00d57fffbec6e3716c4a66213a87e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc11e72d39356bb073d14bb668642545",
+    "url": "/jordan-taylor/index.html"
+  },
+  {
+    "revision": "e9abb38351596bea267c",
+    "url": "/jordan-taylor/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0b1c1b8b93d78535d018",
+    "url": "/jordan-taylor/static/js/2.15931a8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/jordan-taylor/static/js/2.15931a8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9abb38351596bea267c",
+    "url": "/jordan-taylor/static/js/main.e43a1691.chunk.js"
+  },
+  {
+    "revision": "6ca30afac846f2135994",
+    "url": "/jordan-taylor/static/js/runtime-main.10a2d297.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/jordan-taylor/static/media/logo.5d5d9eef.svg"
+  }
+]);
